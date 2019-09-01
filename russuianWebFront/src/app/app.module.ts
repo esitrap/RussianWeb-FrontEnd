552f2bd97c8bb6a82ec304fa0c03e6778b3cde6b
esitrap <esitrap@gmail.com>
2019-09-01 13:42:45 +0430

    jabejaie posts

--- russuianWebFront/src/app/app.module.ts
+++ russuianWebFront/src/app/app.module.ts
@@ -3,21 +3,23 @@ import { NgModule } from '@angular/core';
 import {TableModule} from 'primeng/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { PostsComponent } from './posts/posts.component';
-import { PostDetailComponent } from './posts/post-detail/post-detail.component';
+import {AccordionModule} from 'primeng/accordion';
+import { UsersComponent } from './managerPanel/users/users.component';
+import { PostsComponent } from './managerPanel/postsManager/posts.component';
+import { PostDetailComponent } from './managerPanel/postsManager/post-detailManager/post-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     PostsComponent,
-    PostDetailComponent
+    PostDetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TableModule
+    TableModule,
+     AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
