f5bfa5e3110269f3c0ab875b835222852b2a8f9a
Ali <esitrap@gmail.com>
2019-09-02 22:00:33 +0430

    rafe irad

--- russuianWebFront/src/app/managerPanel/postsManager/posts/posts.component.ts
+++ russuianWebFront/src/app/managerPanel/postsManager/posts/posts.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-postsManager',
-  templateUrl: './posts/posts.component.html',
-  styleUrls: ['./posts/posts.component.css']
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
 
