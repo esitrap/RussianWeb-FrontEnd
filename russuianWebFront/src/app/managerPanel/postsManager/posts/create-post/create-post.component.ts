d3e25c45ca04a9e4439aacd66524c37a27719d91
Ali <esitrap@gmail.com>
2019-09-09 20:34:46 +0430

    eventemitter

--- russuianWebFront/src/app/managerPanel/postsManager/posts/create-post/create-post.component.ts
+++ russuianWebFront/src/app/managerPanel/postsManager/posts/create-post/create-post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsService } from 'src/app/posts.service';
 import { postManager } from '../../Post';
+import { Output, EventEmitter } from '@angular/core';
 
 
 @Component({
@@ -9,30 +10,29 @@ import { postManager } from '../../Post';
   styleUrls: ['./create-post.component.css']
 })
 export class CreatePostComponent implements OnInit {
-  posts: postManager[]
+
   onvan: string;
   tarikheEnteshar: Date;
   kholaseyePost: string;
   matneKamelePost: string;
+  @Output() postCreated = new EventEmitter();
+
 
   constructor(private postsService: PostsService) { }
 
   ngOnInit() {
-    this.getPosts();
+
   }
-  getPosts(): void {
-    this.postsService.getPosts()
-      .subscribe(posts => this.posts = posts)
-  }
+
   add() {
     this.postsService.addPosts({
-      onvan : this.onvan,
-      tarikheEnteshar : this.tarikheEnteshar,
-      kholaseyePost : this.kholaseyePost,
-      matneKamelePost : this.matneKamelePost
+      onvan: this.onvan,
+      tarikheEnteshar: this.tarikheEnteshar,
+      kholaseyePost: this.kholaseyePost,
+      matneKamelePost: this.matneKamelePost
     }).subscribe(
       post => {
-        this.posts.push(post);
+        this.postCreated.emit();
       }
     );
   }
